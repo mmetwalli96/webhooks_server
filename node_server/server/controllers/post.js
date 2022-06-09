5df7c4e9984c746cf9c615b99eea3d46b4545e18
mmetwalli96 <m.metwalli@outlook.com>
2022-06-09 19:11:48 -0400

    Grab the webhook secret to hash and authinticate the request

--- node_server/server/controllers/post.js
+++ node_server/server/controllers/post.js
@@ -1,7 +1,15 @@
 /* define post request handlers here */
 
+// import dependencies
+const { Webhooks } = require('@octokit/webhooks');
+require('dotenv').config();
+
 module.exports = {
     eventHandler: (req, res) => {
+        const webhook = new Webhooks({
+            secret: process.env.WEBHOOK_SECRET,
+        });
+        console.log(webhook)
         console.log(req.headers);
         res.send('Well, it worked!');
         console.log(req.body);

5173b1d028f4772251adee6a37570d29216f28b3
mmetwalli96 <m.metwalli@outlook.com>
2022-06-09 22:59:51 -0400

    Creates the guidance for the executing the right actions based on the recieved payload

--- node_server/server/controllers/post.js
+++ node_server/server/controllers/post.js
@@ -5,13 +5,37 @@ const { Webhooks } = require('@octokit/webhooks');
 require('dotenv').config();
 
 module.exports = {
-    eventHandler: (req, res) => {
+    eventHandler: async (req, res) => {
         const webhook = new Webhooks({
             secret: process.env.WEBHOOK_SECRET,
         });
-        console.log(webhook)
-        console.log(req.headers);
-        res.send('Well, it worked!');
-        console.log(req.body);
+
+	const payload = req.body;
+	
+	const signature = await webhook.sign(payload);
+	
+	const status = await webhook.verify(payload, signature)
+	if (status) {
+		const name = req.headers["x-github-event"];
+		
+		switch(name){
+		case "push":
+			// create git pull request
+			console.log("commit was created")
+			break;
+		case "pull_request":
+			const pullRequestStatus = payload.action;
+			if (pullRequestStatus === "closed"){
+			// create git pull request
+			console.log("pull requested approved")
+			};
+			break;
+		};
+		
+        	res.send("Well, it worked!");
+        }
+	else {
+		res.send(401)
+	}
     },
 };

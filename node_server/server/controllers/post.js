4ecb21bbe02f599263b3959f52f0af00a3855195
mmetwalli96 <m.metwalli@outlook.com>
2022-06-09 23:23:01 -0400

    Reformat the code in post.js to look nicer

--- node_server/server/controllers/post.js
+++ node_server/server/controllers/post.js
@@ -1,41 +1,43 @@
 /* define post request handlers here */
 
 // import dependencies
-const { Webhooks } = require('@octokit/webhooks');
-require('dotenv').config();
+const { Webhooks } = require("@octokit/webhooks");
+require("dotenv").config();
 
 module.exports = {
-    eventHandler: async (req, res) => {
-        const webhook = new Webhooks({
-            secret: process.env.WEBHOOK_SECRET,
-        });
+  eventHandler: async (req, res) => {
+    const webhook = new Webhooks({
+      secret: process.env.WEBHOOK_SECRET,
+    });
 
-	const payload = req.body;
-	
-	const signature = await webhook.sign(payload);
-	
-	const status = await webhook.verify(payload, signature)
-	if (status) {
-		const name = req.headers["x-github-event"];
-		
-		switch(name){
-		case "push":
-			// create git pull request
-			console.log("commit was created")
-			break;
-		case "pull_request":
-			const pullRequestStatus = payload.action;
-			if (pullRequestStatus === "closed"){
-			// create git pull request
-			console.log("pull requested approved")
-			};
-			break;
-		};
-		
-        	res.send("Well, it worked!");
+    try {
+      const signature = await webhook.sign(payload);
+      const status = await webhook.verify(payload, signature);
+
+      if (status) {
+          
+        const payload = req.body;
+        const name = req.headers["x-github-event"];
+
+        switch (name) {
+          case "push":
+            // create git pull request
+            console.log("commit was created");
+            break;
+          case "pull_request":
+            const pullRequestStatus = payload.action;
+            if (pullRequestStatus === "closed") {
+              // create git pull request
+              console.log("pull requested approved");
+            }
+            break;
         }
-	else {
-		res.send(401)
-	}
-    },
+        res.status(200).send("Deployed");
+      } else {
+        res.send(401);
+      }
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
 };
